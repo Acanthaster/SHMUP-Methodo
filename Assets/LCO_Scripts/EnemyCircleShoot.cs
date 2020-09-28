584a00f3716846d85bc8672724cef9d604f0fe94
Lucas <lucas.combes28@gmail.com>
2020-09-28 15:09:20 +0200

    Add Ennemies

---
+++ Assets/LCO_Scripts/EnemyCircleShoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCircleShoot : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+
+    public float fireRate;
+    Vector2 spawnBullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Shoot");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnBullet = new Vector2(transform.position.x - 1, transform.position.y);
+    }
+
+    IEnumerator Shoot()
+    {
+        Instantiate(bullet, spawnBullet, Quaternion.identity);
+        yield return new WaitForSeconds(fireRate);
+        StartCoroutine("Shoot");
+    }
+}
