8b41cab23fa6378f2648e9171d2c2a1f548c3991
Acanthaster <a.remazeilles@rubika-edu.com>
2020-10-16 11:30:12 +0200

    Camera move

---
+++ Assets/ALR_Scripts/ALR_CameraMoveForward.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ALR_CameraMoveForward : MonoBehaviour
+{
+    public float camSpeed = 1f;
+    //public Vector2 camMove;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right.normalized * camSpeed * Time.fixedDeltaTime);
+
+    }
+}
